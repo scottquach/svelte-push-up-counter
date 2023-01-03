b79c4d87b180d8c94c3d7ad77bb035acf34da91f
Scott <scottqglobal@gmail.com>
2023-01-02 20:51:27 -0800

    Screen will be kept awake

--- android/app/src/main/java/com/scottquach/pushupcounter/MainActivity.java
+++ android/app/src/main/java/com/scottquach/pushupcounter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.scottquach.pushupcounter;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.getcapacitor.BridgeActivity;
 import com.scottquach.pushupcounter.plugins.ProximitySensor.ProximitySensorPlugin;
@@ -11,5 +12,6 @@ public class MainActivity extends BridgeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         registerPlugin(ProximitySensorPlugin.class);
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 }
